d2332e570254c150d575b6da79bafef39590c778
Aarush <aarush@happytails.app>
2026-03-15 21:44:59 +0530

    Update all URLs to happytails.vercel.app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,7 @@ const config: CapacitorConfig = {
   webDir: "public",
   server: {
     androidScheme: "https",
-    url: "https://happy-tails-coral.vercel.app",
+    url: "https://happytails.vercel.app",
     cleartext: false,
   },
   plugins: {
